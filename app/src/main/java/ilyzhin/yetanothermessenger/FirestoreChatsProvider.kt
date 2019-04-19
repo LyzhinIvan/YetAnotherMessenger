b600eb72b269d63d27f64931f848768f732f0567
ilyzhin <lyzhin-ivan@yandex.ru>
2019-04-19 15:06:18 +0500

    chat title

--- app/src/main/java/ilyzhin/yetanothermessenger/FirestoreChatsProvider.kt
+++ app/src/main/java/ilyzhin/yetanothermessenger/FirestoreChatsProvider.kt
@@ -2,6 +2,7 @@ package ilyzhin.yetanothermessenger
 
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 
 class FirestoreChatsProvider : AbstractChatsProvider() {
@@ -15,9 +16,16 @@ class FirestoreChatsProvider : AbstractChatsProvider() {
             .addOnSuccessListener { document ->
                 chats.clear()
                 document.let {
-                    (document["chats"] as List<*>).let { chatsRefs ->
+                    (document["chats"] as List<DocumentReference>).let { chatsRefs ->
                         for (chatRef in chatsRefs) {
-                            chats.add(Chat(chatRef.toString()))
+                            chatRef.get()
+                                .addOnSuccessListener { result ->
+                                    chats.add(Chat(result.data!!["title"].toString()))
+                                    callback()
+                                }
+                                .addOnFailureListener { exception ->
+                                    Log.d("MYAPP", "failed to load chat", exception)
+                                }
                         }
 
                     }
@@ -36,6 +44,7 @@ class FirestoreChatsProvider : AbstractChatsProvider() {
     }
 
     override fun getChatsCount(): Int {
+        Log.d("MYAPP", "chats size " + chats.size)
         return chats.size;
     }
 }
